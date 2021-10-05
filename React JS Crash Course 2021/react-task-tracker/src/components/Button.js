6548f78329e67f8867308d016b61f1291f3ec183
Peter Phenow <peter.phenow@gmail.com>
2021-10-05 14:10:44 -0500

    added props, default props, and prop typesto button

--- React JS Crash Course 2021/react-task-tracker/src/components/Button.js
+++ React JS Crash Course 2021/react-task-tracker/src/components/Button.js
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button className='btn'>Add</button>;
+import PropTypes from 'prop-types';
+
+const Button = ({ color, text }) => {
+  return (
+    <button style={{ backgroundColor: color }} className='btn'>
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: 'red',
+  text: 'Default Button'
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string
 };
 
 export default Button;
