05fbda44d6967e3b99488bfc4b6580f79bb19ebb
Peter Phenow <peter.phenow@gmail.com>
2021-10-07 14:52:23 -0500

    hide add task component until we click that we want to add a task

--- React JS Crash Course 2021/react-task-tracker/src/App.js
+++ React JS Crash Course 2021/react-task-tracker/src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -45,7 +46,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask onAdd={addTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} /> : 'No Tasks To Show'}
     </div>
   );
